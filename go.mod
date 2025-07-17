71e6940f676c84201343a45200acdfe443f1eaef
subhamnayak76 <subhamnayak9638@gmail.com>
2025-07-17 22:30:59 +0530

    batman

---
+++ go.mod
@@ -0,0 +1,3 @@
+module snippetbox.subh.am
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter,r*http.Request){
+	w.Write([]byte("hello from snippetbox"))
+}
+
+func snippetview(w http.ResponseWriter,r*http.Request){
+	// w.Write([]byte("display the snippet.."))
+	id,err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1{
+		http.NotFound(w,r)
+		return
+	}
+	msg := fmt.Sprintf("display a specific snippet with id %d..",id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter,r*http.Request){
+	w.Write([]byte("display create a snipptex form"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}",home)
+	mux.HandleFunc("/snippet/view/{id}",snippetview)
+	mux.HandleFunc("/snippet/create",snippetCreate)
+	log.Print("starting server on 4000")
+	
+
+	err := http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
+	
+}
