9967dd8a3181a7f1d36e4e7d396a302759b0d7a2
subhamnayak76 <subhamnayak9638@gmail.com>
2025-07-18 23:48:24 +0530

    logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,31 @@
 package main
+
 import (
-"log"
-"net/http"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
 )
 func main() {
+
+addr := flag.String("addr",":4000","HTTP network address")
+flag.Parse()
+
+logger := slog.New(slog.NewTextHandler(os.Stdout,nil))
 mux := http.NewServeMux()
 
 fileServer := http.FileServer(http.Dir("./ui/static/"))
 mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-mux.HandleFunc("GET /", home)
+mux.HandleFunc("GET /{$}", home)
 mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 mux.HandleFunc("GET /snippet/create", snippetCreate)
 mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-log.Print("starting server on :4000")
-err := http.ListenAndServe(":4000", mux)
-log.Fatal(err)
+
+
+logger.Info("starting server","addr",*addr)
+err := http.ListenAndServe(*addr, mux)
+logger.Error(err.Error())
+
+os.Exit(1)
 }
