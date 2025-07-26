42a03fcdf73484b557a2f2b7cf1f3491cc04e7fb
subhamnayak76 <subhamnayak9638@gmail.com>
2025-07-26 16:29:41 +0530

    get-info

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,42 +5,50 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
-
 )
-func(app *application) home(w http.ResponseWriter, r *http.Request) {
-w.Header().Add("Server", "Go")
-files := []string{
-	"./ui/html/base.tmpl",
-	"./ui/html/partials/nav.tmpl",
-	"./ui/html/pages/home.tmpl",
-}
-ts,err := template.ParseFiles(files...)
-if err != nil {
-	app.serverError(w,r,err)
-	http.Error(w,"internal server error",http.StatusInternalServerError)
-	return
-}
 
-err = ts.ExecuteTemplate(w,"base",nil)
-if err != nil {
-	app.serverError(w,r,err)
-	http.Error(w,"internal server error",http.StatusInternalServerError)
-}
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 
-w.Write([]byte("Hello from Snippetbox"))
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, r, err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
+
+	w.Write([]byte("Hello from Snippetbox"))
 }
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-id, err := strconv.Atoi(r.PathValue("id"))
-if err != nil || id < 1 {
-http.NotFound(w, r)
-return
-}
-fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-w.Write([]byte("Display a form for creating a new snippet..."))
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
-func (app *application)snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-w.WriteHeader(http.StatusCreated)
-w.Write([]byte("Save a new snippet..."))
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -3,7 +3,10 @@ package models
 import (
 	"database/sql"
 	"time"
+	"errors"
 )
+
+var ErrNoRecord = errors.New("models: no matching record found")
 
 type Snippet struct {
 	ID int
@@ -19,13 +22,55 @@ type SnippetModel struct {
 
 
 func (m *SnippetModel) Insert(title string ,content string ,expires int) (int ,error){
-	return 0,nil 
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result ,err := m.DB.Exec(stmt,title,content,expires)
+	if err != nil {
+		return 0,err
+	}
+
+	id,err := result.LastInsertId()
+	if err != nil {
+		return 0,err
+	}
+
+	return int(id),nil
+
+}
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+// Write the SQL statement we want to execute. Again, I've split it over two
+// lines for readability.
+stmt := `SELECT id, title, content, created, expires FROM snippets
+WHERE expires > UTC_TIMESTAMP() AND id = ?`
+// Use the QueryRow() method on the connection pool to execute our
+// SQL statement, passing in the untrusted id variable as the value for the
+// placeholder parameter. This returns a pointer to a sql.Row object which
+// holds the result from the database.
+row := m.DB.QueryRow(stmt, id)
+// Initialize a new zeroed Snippet struct.
+var s Snippet
+// Use row.Scan() to copy the values from each field in sql.Row to the
+// corresponding field in the Snippet struct. Notice that the arguments
+// to row.Scan are *pointers* to the place you want to copy the data into,
+// and the number of arguments must be exactly the same as the number of
+// columns returned by your statement.
+err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+if err != nil {
+// If the query returns no rows, then row.Scan() will return a
+// sql.ErrNoRows error. We use the errors.Is() function check for that
+// error specifically, and return our own ErrNoRecord error
+// instead (we'll create this in a moment).
+if errors.Is(err, sql.ErrNoRows) {
+return Snippet{}, ErrNoRecord
+} else {
+return Snippet{}, err
+}
+}
+// If everything went OK, then return the filled Snippet struct.
+return s, nil
 }
 
-
-func (m *SnippetModel) Get(id int) (Snippet,error){
-	return Snippet{},nil
-}
 
 func (m *SnippetModel) Latest() ([]Snippet,error){
 	return nil ,nil 
