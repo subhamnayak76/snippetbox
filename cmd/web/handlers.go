a74154e4ec89dd7fa49b3aa79b7ba8d2f4b30fbd
subhamnayak76 <subhamnayak9638@gmail.com>
2025-07-26 17:10:16 +0530

    snippet-view

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,55 +5,77 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"strconv"
 	"snippetbox.subh.am/internal/models"
+	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
-
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+	// w.Header().Add("Server", "Go")
+	// files := []string{
+	// 	"./ui/html/base.tmpl",
+	// 	"./ui/html/partials/nav.tmpl",
+	// 	"./ui/html/pages/home.tmpl",
+	// }
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	http.Error(w, "internal server error", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	http.Error(w, "internal server error", http.StatusInternalServerError)
+	// }
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-id, err := strconv.Atoi(r.PathValue("id"))
-if err != nil || id < 1 {
-http.NotFound(w, r)
-return
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+	// Initialize a slice containing the paths to the view.tmpl file,
+	// plus the base layout and navigation partial that we made earlier.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+	// Parse the template files...
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	// And then execute them. Notice how we are passing in the snippet
+	// data (a models.Snippet struct) as the final parameter?
+	err = ts.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
-// Use the SnippetModel's Get() method to retrieve the data for a
-// specific record based on its ID. If no matching record is found,
-// return a 404 Not Found response.
-snippet, err := app.snippets.Get(id)
-if err != nil {
-if errors.Is(err, models.ErrNoRecord) {
-http.NotFound(w, r)
-} else {
-app.serverError(w, r, err)
-}
-return
-}
-// Write the snippet data as a plain-text HTTP response body.
-fmt.Fprintf(w, "%+v", snippet)
-}
-
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
