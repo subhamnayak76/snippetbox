4df039b99ced994d3459f40b28a63f717a9610a2
subhamnayak76 <subhamnayak9638@gmail.com>
2025-07-27 17:54:06 +0530

    panic-recover

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,16 @@
 package main
+
 import "net/http"
 
 func (app *application) routes() http.Handler {
-mux := http.NewServeMux()
-fileServer := http.FileServer(http.Dir("./ui/static/"))
-mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-mux.HandleFunc("GET /{$}", app.home)
-mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	return commonHeaders(mux)
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	
+	return app.recoverPanic(app.LogRequest(commonHeaders(mux)))
 }
+
